dc07953f4f0a81169e36e1de396fe10f483bb64e
xcc3641 <446022570@qq.com>
2016-08-09 20:26:08 +0800

    分离缓存，单一原则

--- ImageLoader/src/main/java/com/hugo/imageloader/ImageLoader.java
+++ ImageLoader/src/main/java/com/hugo/imageloader/ImageLoader.java
@@ -2,7 +2,6 @@ package com.hugo.imageloader;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.util.LruCache;
 import android.widget.ImageView;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -12,33 +11,16 @@ import java.util.concurrent.Executors;
 /**
  * Created by HugoXie on 16/8/9.
  *
- * Email: Hugo3641@gamil.com
+ * Email: Hugo3641@gmail.com
  * GitHub: https://github.com/xcc3641
  * Info: 学习 Android 设计模式做 图片加载器 裤子
  */
 public class ImageLoader {
 
     // 图片缓存
-    LruCache<String, Bitmap> mImageCache;
+    ImageCache mImageCache = new ImageCache();
     // 线程池,线程数量为 CPU 数量
     ExecutorService mExecutorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-
-    public ImageLoader() {
-        initImageCache();
-    }
-
-    private void initImageCache() {
-        // 计算可使用的最大内存
-        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
-        // 取 1/4 的可用内存作为缓存
-        final int cacheSize = maxMemory / 4;
-        mImageCache = new LruCache<String, Bitmap>(cacheSize) {
-            @Override
-            protected int sizeOf(String key, Bitmap value) {
-                return value.getRowBytes() * value.getHeight() / 1024;
-            }
-        };
-    }
 
     public void displayImage(final String url, final ImageView imageView) {
         imageView.setTag(url);
@@ -67,7 +49,6 @@ public class ImageLoader {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         return bitmap;
     }
 }
